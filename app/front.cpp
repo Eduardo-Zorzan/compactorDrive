036ff761dba0293c218f53a434d2b879d949544b
Eduardo Botton Zorzan <duduzorzan@gmail.com>
2024-11-27 22:52:34 -0300

    test

--- app/front.cpp
+++ app/front.cpp
@@ -18,7 +18,7 @@ namespace MyApp {
         window_flags |= ImGuiWindowFlags_NoMove;
         ImGui::Begin("Settings", p_open, window_flags);
         return window_flags;
-        //test
+        //testsad
     }
 
     void menuBar()
